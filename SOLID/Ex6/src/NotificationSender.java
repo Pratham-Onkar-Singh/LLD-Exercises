1477b07487a3e5850f54e1192fce3666d8ad91fb
Pratham Onkar Singh <prathamaj09@gmail.com>
2026-02-23 12:11:02 +0530

    complete Ex-6 c

--- SOLID/Ex6/src/NotificationSender.java
+++ SOLID/Ex6/src/NotificationSender.java
@@ -1,5 +1,9 @@
 public abstract class NotificationSender {
     protected final AuditLog audit;
+
     protected NotificationSender(AuditLog audit) { this.audit = audit; }
+
+    public boolean canSend(Notification n) { return true; }
+
     public abstract void send(Notification n);
 }
